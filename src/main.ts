9458c78a52e19c648302b80bdb31eca2240c392b
Giulio Parrucci <giuparrucc@gmail.com>
2026-03-19 10:45:50 +0100

    Added ribbon icon

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Plugin } from 'obsidian';
 import { DEFAULT_SETTINGS } from "./settings";
 import { BMSettingTab } from "./ui/settings-tab"; 
 import { BMSettings } from "./types";
+import { isConfigValid } from 'utils/validation';
 
 export default class BacklinkMigrator extends Plugin {
 
@@ -10,6 +11,15 @@ export default class BacklinkMigrator extends Plugin {
     async onload() {
         await this.loadSettings();
         this.addSettingTab(new BMSettingTab(this.app, this));
+        
+        this.addRibbonIcon("scan-text", "Run manual scan for Backlink Migrator", (event: MouseEvent) => {
+            if (!isConfigValid(this.settings)) {
+                return;
+            }
+            // TODO: metodo di scan
+        })
+
+
     }
 
     async onunload() {
---
+++ src/utils/validation.ts
@@ -0,0 +1,19 @@
+import { Notice } from "obsidian";
+import { BMSettings } from "types";
+
+export function isConfigValid(settings: BMSettings): boolean {
+
+    if (!settings.targetFolder || settings.targetFolder === '') {
+        new Notice("Backlink Migrator: Target folder is missing. Please check settings.");
+        return false;
+    }
+    if (!settings.sourceFolders || settings.sourceFolders.length === 0) {
+        new Notice("Backlink Migrator: No source folders selected. Please check settings");
+        return false;
+    }
+    if (!settings.threshold || settings.threshold <= 0) {
+        new Notice("Backlink Migrator: Threshold value not valid. Please check settings");
+        return false;
+    }
+    return true;
+}
