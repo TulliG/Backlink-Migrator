e795bd4b86749be7b3c2fdc5d61d63bb998fb265
Giulio Parrucci <giuparrucc@gmail.com>
2026-04-11 17:05:56 +0200

    settings ui fix

--- src/ui/settings-tab.ts
+++ src/ui/settings-tab.ts
@@ -99,6 +99,8 @@ export class BMSettingTab extends PluginSettingTab {
 					});
 			});
 
+		containerEl.createEl("hr");
+
 		// add source folder button
 		new Setting(containerEl)
 			.setHeading()
